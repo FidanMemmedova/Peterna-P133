0974cdc9793e8b1d4081ccc9070872e4b5ad8308
FidanMemmedova <5TAWDL9@code.edu.az>
2023-01-28 19:53:54 +0400

    salam

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using peterna.DAL;
+using peterna.Models;
+
+namespace peterna.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+         
+        public IActionResult Index()
+        {
+            List<Service> services=_context.Services.ToList();
+            return View(services);
+        }
+    }
+}
---
+++ Models/Service.cs
@@ -0,0 +1,11 @@
+﻿namespace peterna.Models
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string ServiceName { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+    }
+}
